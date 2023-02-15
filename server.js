542982a3351427f4d1338350e3f206e93dfe9393
Isaiah Winegarden <groovygabriel26@gmail.com>
2023-02-15 10:55:32 -0600

    Created the newFolderName function

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
 const newFolderName = function (folders) {
-
+  const count = folders.length + 1
+  console.log('New Folder (' + count + ')')
 }
+
+newFolderName(['New Folder', 'New Folder (2)', 'New folder (3)'])
+
+// The console log comes out to "New Folder (4)"
